b03fb137d8289278dffcd8bf3d65fe37666839c0
Tyler <tyler.freebery@gmail.com>
2020-12-07 20:38:45 -0500

    Assignment 2 and 3 done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,21 @@
+//Assignment One 
 let country = 'USA';
 let continent = 'North America';
-let population = "331 Million";
+let population = 1000;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Assignment Two
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//Assignment Three
+language = 'English';
+const numOfState = 50;
+console.log(language);
