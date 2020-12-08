e47c682c41241a5a029566c957e66b0e0c797042
Tyler <tyler.freebery@gmail.com>
2020-12-07 21:09:46 -0500

    More Assignments completed

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -19,3 +19,13 @@ console.log(typeof language);
 language = 'English';
 const numOfState = 50;
 console.log(language);
+
+//Assignment Four
+console.log(population / 2);
+population++;
+console.log(population)
+console.log(population > 6);
+console.log(population < 33);
+const description1 = country + ' is in ' + continent + ' and its population is ' + population + ' and the people mainly speak ' + language;
+console.log(description1);
+
