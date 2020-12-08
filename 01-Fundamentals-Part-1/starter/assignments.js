ed9feb98f9cae91dd9c0e47b4fbb7c0f4f079508
Tyler <tyler.freebery@gmail.com>
2020-12-08 15:51:44 -0500

    Commented out unused code

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,31 +1,37 @@
-//Assignment One 
-let country = 'USA';
-let continent = 'North America';
-let population = 1000;
+//Assignment One
+// let country = 'USA';
+// let continent = 'North America';
+// let population = 1000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 //Assignment Two
-let isIsland = false;
-let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
 //Assignment Three
-language = 'English';
-const numOfState = 50;
-console.log(language);
+// language = 'English';
+// const numOfState = 50;
+// console.log(language);
 
 //Assignment Four
-console.log(population / 2);
-population++;
-console.log(population)
-console.log(population > 6);
-console.log(population < 33);
-const description1 = country + ' is in ' + continent + ' and its population is ' + population + ' and the people mainly speak ' + language;
-console.log(description1);
+// console.log(population / 2);
+// population++;
+// console.log(population)
+// console.log(population > 6);
+// console.log(population < 33);
+// const description1 = country + ' is in ' + continent + ' and its population is ' + population + ' and the people mainly speak ' + language;
+// console.log(description1);
 
+
+// let now = 2020;
+// let tyAge = now - 1993;
+// let newAge = now - 2000;
+
+// console.log(tyAge > newAge);
